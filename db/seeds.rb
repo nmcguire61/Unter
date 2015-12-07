23b0cdcf78af5db5d5f4e1f7dc2b4e17bc62b870
Vismajor <podobaszalaizsolt@gmail.com>
2015-12-07 16:38:03 +0000

    changes to profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
 
 
-u1 = User.create!( name: "admin", email:"admin@unter.com", password: "password", role: "admin")
-u2 = User.create!( name: "user", email:"user@unter.com", password: "password", role: "user")
+u1 = User.create!( name: "admin", email:"admin@unter.com", password: "password", role: "admin", rating_avg: 9.76)
+u2 = User.create!( name: "user", email:"user@unter.com", password: "password", role: "user", description: "Hey guys, I like to travel a lot, lets save monies on gas price. Whoo! (Neil)")
