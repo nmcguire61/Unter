adf8e83fcf8c8e7df0f8f36f8771e70b8a788d9e
NeilMcGuire <nmcguire61@gmail.com>
2015-12-09 14:42:14 +0000

    changing base class for payment_transactions_controller

--- app/controllers/payment_transactions_controller.rb
+++ app/controllers/payment_transactions_controller.rb
@@ -1,4 +1,4 @@
-class PaymentTransactionsController < Devise::RegistrationsController
+class PaymentTransactionsController < ApplicationController
   # load_and_authorize_resource
   before_action :set_payment_transaction, only: [:show, :edit, :update, :destroy]
 
